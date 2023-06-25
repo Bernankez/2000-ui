f3723432a0f2e4111daeee444c061911d6381017
cole <bernankeic@gmail.com>
2023-06-25 14:11:18 +0800

    chore: init

---
+++ packages/components/vite.config.ts
@@ -0,0 +1,58 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import VueJsx from "@vitejs/plugin-vue-jsx";
+import dts from "vite-plugin-dts";
+
+const componentsDir = __dirname;
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      Vue(),
+      VueJsx(),
+      dts({
+        // 声明文件输出目录
+        outputDir: "es",
+        // 入口文件的跟路径
+        entryRoot: componentsDir,
+        // exclude auto inferred from tsconfig
+        cleanVueFileName: true,
+      }),
+      dts({
+        outputDir: "lib",
+        entryRoot: componentsDir,
+        cleanVueFileName: true,
+      }),
+    ],
+    build: {
+      minify: false,
+      lib: {
+        name: "2000-ui",
+        entry: resolve(componentsDir, "index.ts"),
+      },
+      rollupOptions: {
+        external: ["vue"],
+        output: [
+          {
+            format: "es",
+            entryFileNames: chunkInfo => `${chunkInfo.name.replace(/.vue$/, "")}.mjs`,
+            // 打包目录和文件目录对应
+            preserveModules: true,
+            // 入口文件的根路径
+            preserveModulesRoot: componentsDir,
+            // 打包输出目录
+            dir: "es",
+          },
+          {
+            format: "cjs",
+            entryFileNames: chunkInfo => `${chunkInfo.name.replace(/.vue$/, "")}.cjs`,
+            preserveModules: true,
+            preserveModulesRoot: componentsDir,
+            dir: "lib",
+          },
+        ],
+      },
+    },
+  };
+});
