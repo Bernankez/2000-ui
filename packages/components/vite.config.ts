1423e2528cfc3f4e501ce76e40b5ca7273f1e1ff
cole <bernankeic@gmail.com>
2023-06-28 15:29:33 +0800

    feat: preserve css files

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,13 +1,36 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import type { PreRenderedAsset } from "rollup";
 import Vue from "@vitejs/plugin-vue";
 import VueJsx from "@vitejs/plugin-vue-jsx";
 import dts from "vite-plugin-dts";
 import Inspect from "vite-plugin-inspect";
 
+const INTERNAL_SOURCE = "/* vite internal call, ignore */";
+
 const componentsDir = __dirname;
 
 export default defineConfig(() => {
+  // Map<relativePathName, fileName>
+  let assetMap = new Map<string, string>();
+
+  function assetFileNames(assetInfo: PreRenderedAsset): string {
+    if (assetInfo.source === INTERNAL_SOURCE && assetInfo.name) {
+      assetMap.set(assetInfo.name, assetInfo.name.split("/").pop() || assetInfo.name);
+    } else {
+      for (const [relativePathName, fileName] of assetMap) {
+        if (fileName === assetInfo.name) {
+          const path = relativePathName.split("/");
+          path.pop();
+          assetMap.delete(relativePathName);
+          const name = fileName.replace(/(.*)(?=\.vue).*/, "$1.css");
+          return `${path.join("/")}/${name}`;
+        }
+      }
+    }
+    return assetInfo.name!;
+  }
+
   return {
     plugins: [
       Vue(),
@@ -29,6 +52,12 @@ export default defineConfig(() => {
         build: true,
         outputDir: ".vite-inspect",
       }),
+      {
+        name: "vite:cleanup",
+        buildStart() {
+          assetMap = new Map();
+        },
+      },
     ],
     build: {
       minify: false,
@@ -43,6 +72,8 @@ export default defineConfig(() => {
           {
             format: "es",
             entryFileNames: chunkInfo => `${chunkInfo.name.replace(/.vue$/, "")}.mjs`,
+            // 按目录结构输出css文件
+            assetFileNames,
             // 打包目录和文件目录对应
             preserveModules: true,
             // 入口文件的根路径
@@ -53,6 +84,7 @@ export default defineConfig(() => {
           {
             format: "cjs",
             entryFileNames: chunkInfo => `${chunkInfo.name.replace(/.vue$/, "")}.cjs`,
+            assetFileNames,
             preserveModules: true,
             preserveModulesRoot: componentsDir,
             dir: "lib",
