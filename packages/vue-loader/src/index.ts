b43b623cd99f11f4281ddafc6ff0eefb6969a251
cole <bernankeic@gmail.com>
2023-07-27 15:03:26 +0800

    fix(vue-loader): fileURLToPath

--- packages/vue-loader/src/index.ts
+++ packages/vue-loader/src/index.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import { fileURLToPath } from "node:url";
-import { type Load, type Resolve, normalizePath } from "./utils";
+import { type Load, type Resolve } from "./utils";
 import { compileTsCode, compileVueCode } from "./compile";
 
 export const resolve: Resolve = async (specifier, context, defaultResolve, recursiveCall) => {
@@ -17,9 +17,8 @@ export const resolve: Resolve = async (specifier, context, defaultResolve, recur
 export const load: Load = async (url, context, defaultLoad) => {
   const content = await defaultLoad(url, context, defaultLoad);
   if (context.format === "vue") {
-    const source = fs.readFileSync(fileURLToPath(url), "utf-8");
-    // remove file:// flag
-    const path = normalizePath(url.replace("file://", ""));
+    const path = fileURLToPath(url);
+    const source = fs.readFileSync(path, "utf-8");
     let { content: code, ext } = compileVueCode(source, path);
 
     if (ext === "ts") {
