50aacb122403661ee4ab043530212eba4428e03c
cole <bernankeic@gmail.com>
2023-07-28 10:55:06 +0800

    chore: fix pre-publish script

--- scripts/pre-publish.ts
+++ scripts/pre-publish.ts
@@ -2,5 +2,6 @@ import { defineBuild } from "./build/excute/define-build";
 import { $ } from "./build/excute";
 
 defineBuild(async () => {
-  await $("copy", { successMessage: "README copied" });
+  await $("pnpm copy", { successMessage: "README copied" });
+  await $("git commit --all -m \"docs: update README\"", { successMessage: "committed" });
 });
