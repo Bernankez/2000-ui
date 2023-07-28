0fa6f80aa8b8afd631fb380bb88c90bc19d72f01
cole <bernankeic@gmail.com>
2023-07-28 16:11:57 +0800

    fix: fix global registry component name

--- packages/utils/src/vue.ts
+++ packages/utils/src/vue.ts
@@ -1,11 +1,11 @@
-import { camelCase, kebabCase, upperCase } from "lodash-es";
+import { camelCase, kebabCase, upperFirst } from "lodash-es";
 import type { App, DefineComponent } from "vue";
 
 export function withInstall(components: Record<string, DefineComponent>) {
   return function install(app: App) {
     Object.keys(components).forEach((name) => {
       const component = components[name];
-      app.component(upperCase(camelCase(name)), component); // ButtonGroup
+      app.component(upperFirst(camelCase(name)), component); // ButtonGroup
       app.component(kebabCase(name), component); // button-group
     });
   };
