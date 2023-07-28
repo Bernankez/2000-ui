3e0538266a351859904e7d99e8723c5445c0cb12
cole <bernankeic@gmail.com>
2023-07-28 16:13:29 +0800

    build: auto commit and push README before release

--- scripts/copy-readme.ts
+++ scripts/copy-readme.ts
@@ -1,9 +1,25 @@
 import { resolve } from "node:path";
-import { copyFileSync } from "fs";
+import { copyFileSync, readFileSync } from "fs";
+import { execSync } from "node:child_process";
+import chalk from "chalk";
 import { getDirname } from "./utils";
 
 const root = resolve(getDirname(import.meta.url), "..");
-const rootReadme = resolve(root, "README.md");
-const componentReadme = resolve(root, "./packages/components/README.md");
+const rootReadmePath = resolve(root, "README.md");
+const componentReadmePath = resolve(root, "./packages/components/README.md");
 
-copyFileSync(rootReadme, componentReadme);
+export function copyReadme() {
+  const rootReadme = readFileSync(rootReadmePath, "utf-8");
+  const componentReadme = readFileSync(componentReadmePath, "utf-8");
+  if (rootReadme !== componentReadme) {
+    copyFileSync(rootReadme, componentReadme);
+    console.info(`${chalk.bgGreen(" Success ")} README copied`);
+    execSync(`git commit ${componentReadmePath} -m "docs: update README"`);
+    console.info(`${chalk.bgGreen(" Success ")} git committed`);
+    execSync("git push");
+    console.info(`${chalk.bgGreen(" Success ")} git pushed`);
+  } else {
+    console.info(`${chalk.bgGreen(" Success ")} Nothing to change`);
+  }
+}
+
--- scripts/pre-publish.ts
+++ scripts/pre-publish.ts
@@ -1,12 +1,3 @@
-import { execSync } from "node:child_process";
-import { defineBuild } from "./build/excute/define-build";
-import { $ } from "./build/excute";
+import { copyReadme } from "./copy-readme";
 
-defineBuild(async () => {
-  await $("pnpm copy", { successMessage: "README copied" });
-  const diff = execSync("git diff --exit-code").toString();
-  if (diff) {
-    await $("git commit --all -m \"docs: update README\"", { successMessage: "committed" });
-    await $("git push", { successMessage: "pushed" });
-  }
-});
+copyReadme();
